be4e9c9d82b71f40b87f8e5f74f9cb91d81b4a5d
Ruben <Ruben@172.24.45.17>
2019-11-18 13:24:24 -0600

    added fxml file and modified main.java

--- MemBibleApp/src/application/Main.java
+++ MemBibleApp/src/application/Main.java
@@ -1,21 +1,20 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("/application/view/AppView.fxml"));
+			primaryStage.setScene(new Scene(root, 800, 800));
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(Exception e){
 			e.printStackTrace();
 		}
 	}
