64787a315dfb1ecd91591f438339d618c8c29c59
Ruben <Ruben@Sams-Laptop>
2019-12-02 16:47:30 -0600

    i did so much.. ded

--- MemBibleApp/src/application/controller/MemBibleController.java
+++ MemBibleApp/src/application/controller/MemBibleController.java
@@ -3,7 +3,6 @@ package application.controller;
 import java.io.IOException;
 
 import application.model.Bible;
-import application.model.Book_Chap_Verse;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,20 +11,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MemBibleController {
+	
 	@FXML
 	public TextArea memVerse;
 	public Text book_Chap_Verse;
 	public Label memVerseTimeCount;
 	public Button book, chapter, verse;
-	public Bible asv = new Bible();
-	public TabPane readTab;
+	public Pane read, select;
+	public SplitPane split;
 	
+	public Bible asv;
 	@FXML
 	public void changeViewDV(ActionEvent event) throws IOException {
 		
@@ -66,43 +68,39 @@ public class MemBibleController {
 		window.show();
 		
 	}
-	
+		
 	@FXML
-	public void changeViewRD(ActionEvent event) throws IOException {
-		compileVerse();
-		Parent newView = FXMLLoader.load(getClass().getResource("/application/view/ReadDisplay.fxml"));
-		Scene newViewScene =  new Scene(newView);
-		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-		window.setScene(newViewScene);
-		window.show();
-		displayRead();
-	}
-	
-	@FXML
-	public void compileBook() {
+	public void compileBook(ActionEvent event) {
 		book_Chap_Verse.setText(book.getText());
 	}
 	
 	@FXML
-	public void compileChap() {
+	public void compileChap(ActionEvent event) {
 		book_Chap_Verse.setText(book_Chap_Verse.getText() + " " + chapter.getText());
 	}
 	
 	@FXML
-	public void compileVerse() {
+	public void compileVerse(ActionEvent event) throws IOException {
 		book_Chap_Verse.setText(book_Chap_Verse.getText() + ":" + verse.getText());
+		read.toFront();
+		read.setOpacity(1);
+		select.setOpacity(0);
+		select.toBack();
+		asv = new Bible();
+		memVerse.setText(asv.loadBible());
 	}
 	
 	@FXML
-	public void displayRead() throws IOException {
+	public void compileVerseMem(ActionEvent event) throws IOException {
+		book_Chap_Verse.setText(book_Chap_Verse.getText() + ":" + verse.getText());
+		split.toFront();
+		split.setOpacity(1);
+		select.setOpacity(0);
+		select.toBack();
+		asv = new Bible();
 		asv.loadBible();
-		for(Book_Chap_Verse bCV: asv.getBible()) {
-			if(bCV.getBook_chap_verse().compareToIgnoreCase(book_Chap_Verse.getText()) == 1) {
-				memVerse.setText(memVerse.getText() + bCV.verse);
-			}
-		}
+		memVerse.setText(asv.setTextVerse(book_Chap_Verse.getText()));
 	}
-	
 	
 	@FXML
 	public void goHome(ActionEvent event) throws IOException {
--- MemBibleApp/src/application/model/Bible.java
+++ MemBibleApp/src/application/model/Bible.java
@@ -7,23 +7,38 @@ import java.util.Scanner;
 
 public class Bible {
 	public ArrayList<Book_Chap_Verse> bible = new ArrayList<Book_Chap_Verse>();
+//	public String bibleR = "";
 	
-	public void loadBible() throws IOException {
-		Scanner bibleFile = new Scanner(new File("src/BibleData/bibles.csv"));
+	public String loadBible() throws IOException {
+		String bibleR = "";
+		Scanner bibleFile = new Scanner(new File("src/BibleData/John.csv"));
 		while(bibleFile.hasNext()) {
-			String[] tokens = bibleFile.nextLine().split(",");
-			if(tokens[0].isEmpty()) {
+			String[] tokens = bibleFile.nextLine().split(",", 2);
 				String book_chap_verse = tokens[0];
 				String verse = tokens[1];
+				bibleR = bibleR + ("\n" + tokens[0] + ": " + tokens[1]);
 				Book_Chap_Verse verses = new Book_Chap_Verse(book_chap_verse, verse);
 				bible.add(verses);
-			}	
+				
 		}
+
 		bibleFile.close();
+		return bibleR;
 	}
 	
 	
-	
+//	public String getBibleR() {
+//		return bibleR;
+//	}
+//
+//
+//
+//	public void setBibleR(String bibleR) {
+//		this.bibleR = bibleR;
+//	}
+
+
+
 	public ArrayList<Book_Chap_Verse> getBible() {
 		return bible;
 	}
@@ -34,9 +49,14 @@ public class Bible {
 		this.bible = bible;
 	}
 
-
-
-	public ArrayList<String> getVerse(String book_chap_verse) {
-		return null;
+	public String setTextVerse(String ver) {
+		String yo = "";
+		for(Book_Chap_Verse item: bible) {
+			if(ver.compareToIgnoreCase(item.getBook_chap_verse()) == 0) {
+				yo = item.getBook_chap_verse() + ": " + item.getVerse();
+			}
+			
+		}
+		return yo;
 	}
 }
